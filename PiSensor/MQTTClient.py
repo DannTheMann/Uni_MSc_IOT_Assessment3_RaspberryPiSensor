20b38afcbe85a52a567b21faedb1a566dc780cef
Daniel Andrews <dja33@kent.ac.uk>
2017-04-13 20:29:17 +0000

    Threading elements added

--- PiSensor/MQTTClient.py
+++ PiSensor/MQTTClient.py
@@ -1,5 +1,5 @@
 
-import time, threading
+import time, threading, traceback
 # MQTT Library, speficially client
 import paho.mqtt.client as mqtt
 # Pi GPIO Control
@@ -13,7 +13,7 @@ __MQTT_TOPIC_CONTROL__ = "/testing/dja33/public/control"
 __MQTT_TOPIC_MESSAGE__ = "/testing/dja33/public/message"
 # MQTT Control messages
 __CONTROL_START_MSG__ = "START"
-__CONTROL_END_MSG     = "END"
+__CONTROL_END_MSG__   = "END"
 __CONTROL_ALARM_MSG__ = "ALARM"
 
 # GPIO settings
@@ -28,48 +28,54 @@ GPIOenabled = True # GPIO interrupt enabled, yes or no
 GPIObouncetime = 300 # How long to wait before accepting new interrupts (Milliseconds)
 GPIOthreshold = 3 # How many consecutive signals we must receive to trigger our state
 GPIOdelaycounter = 0.5 # Seconds between each decrement of a 'count'
-GPIOtimer = none # Object wrapper for Thread manipulation of counting
+GPIOtimer = None # Object wrapper for Thread manipulation of counting
 
 class GPIOTimer:
 
-	_stage = 0
-	_delay = GPIOdelaycounter
-	_thread = none
-	_count = 5
+	# defines that these are the ONLY self referencing fields
+	# for this class/object
+	__slots__ = ("_stage", "_delay", "_thread", "_count")
+
+	def __init__(self):
+		self._delay = GPIOdelaycounter
+		self._stage = 0
+		self._thread = None
+		self._count = 5	
 
 	def _count():
-		if _stage > 0:
-			time.sleep(GPIObouncetime + GPIOdelaycounter)
-			if _count > 0:
-				_count = _count - 1
-			if _count == 0:
-				_count = 5
-				_stage = _stage - 1
+		if self._stage > 0:
+			time.sleep(GPIObouncetime + self._delay)
+			if self._count > 0:
+				self._count = self._count - 1
+			if self._count == 0:
+				self._count = 5
+				self._stage = self._stage - 1
 
-	def start():
-		stop()
-		_thread = threading.Thread(target=_count) 
+	def start(self):
+		self.stop()
+		self._thread = threading.Thread(target=self._count) 
 
-	def stop():
-		if not _thread == none and _thread.is_alive():
-			_thread.stop()
-		_count = 5
-		_stage = 0
+	def stop(self):
+		if not self._thread == None and self._thread.is_alive():
+			self._thread.stop()
+		self._count = 5
+		self._stage = 0
 
-	def increaseSensitivity():
-		_delay = _delay + 0.1
-		_count = _count + 1
+	def increase_sensitivity(self):
+		self._delay = self._delay + 0.1
+		self._count = self._count + 1
 
-	def decreaseSensitivity():
-		_delay = _delay - 1
-		_count = _count - 1
+	def decrease_sensitivity(self):
+		self._delay = self._delay - 1
+		self._count = self._count - 1
 
-	def increment():
-		_stage = _stage + 1
-		if _stage >= GPIOthreshold:
-			publishMessage(__CONTROL_ALARM_MSG__)
-			_stage = 0
-		_count = 5
+	def increment(self):
+		print ("Increment Received: {}".format(self._stage))
+		self._stage = self._stage + 1
+		if self._stage >= GPIOthreshold:
+			publish_message(__CONTROL_ALARM_MSG__)
+			self._stage = 0
+		self._count = 5
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -77,7 +83,7 @@ def on_connect(client, userdata, flags, rc):
     	# Subscribing in on_connect() means that if we lose the connection and
     	# reconnect then subscriptions will be renewed.
 	client.subscribe(__MQTT_TOPIC_CONTROL__)
-	publishMessage(__CONTROL_START_MSG__)
+	publish_message(__CONTROL_START_MSG__)
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
@@ -90,30 +96,29 @@ def on_message(client, userdata, msg):
 	print ("Result: {}".format(result))
 
 def on_noise_break(channel):  
-	# publish message to control that we've been triggered.
 	GPIOtimer.increment()
 
-def publishMessage(msg):
+def publish_message(msg):
 	(result, mid) = client.publish(__MQTT_TOPIC_MESSAGE__, msg, 1, True)
 	print ("Mid: {} | Result: {} | Contents: {}".format(mid, result, msg))
 
-def disableInterrupts():
+def disable_interrupts():
 	print ("Disabling interrupts.")
 	GPIO.remove_event_detect(__GPIO_PIN__) 
 	GPIOtimer.stop()
 	# ...
 
-def enableInterrupts(bounce):
+def enable_interrupts(bounce):
 	print ("Enabling interrupts.")
 	GPIO.add_event_detect(__GPIO_PIN__, GPIO.FALLING, callback=on_noise_break, bouncetime=bounce)
 	GPIOtimer.start()
 	# ...
 
-def updateInterruptSettings(bounce):
-	disableInterrupts()
+def update_interrupt_settings(bounce):
+	disable_interrupts()
 	# Let GPIO catch up
 	time.sleep(3)
-	enableInterrupts(bounce)
+	enable_interrupts(bounce)
 
 # Start 
 try:
@@ -131,8 +136,8 @@ try:
 	print ("Setting up GPIO pins...")
 	GPIO.setmode(GPIO.BOARD)
 	GPIO.setup(__GPIO_PIN__, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
-	GPIOthread = GPIOTimer()
-	enableInterrupts(GPIObouncetime)	
+	GPIOtimer = GPIOTimer()
+	enable_interrupts(GPIObouncetime)	
 
 	print ("Ready to receive/transmit. CRTL+C to terminate.")
 
@@ -141,9 +146,14 @@ try:
 	client.loop_forever()
 
 # CRTL + C
-except:
-	publishMessage(__CONTROL_END_MSG__)
-	disableInterrupts()
-	GPIO.cleanup()       # clean up GPIO on CTRL+C exit
-	print(" Keyboard interrupt, exiting...")
-	quit()
+except KeyboardInterrupt:
+	print(" Keyboard interrupt, exiting...") 
+except BaseException as e:
+	print(" An error occurred. Exiting...") 
+	print (e)
+	traceback.print_exc()
+
+publish_message(__CONTROL_END_MSG__)
+disable_interrupts()
+GPIO.cleanup()       # clean up GPIO on CTRL+C exit
+quit()
